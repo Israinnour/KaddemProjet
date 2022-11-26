6ce62697c2be028bcee54f45d6f7db3f2f079c36
Israinnour <israa.neji8@gmail.com>
2022-11-26 16:09:06 +0100

    first commit

---
+++ src/main/java/tn/esprit/kaddemprojet/entities/Etudiant.java
@@ -0,0 +1,25 @@
+package tn.esprit.kaddemprojet.entities;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table( name = " Etudiant")
+public class Etudiant implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idEtudiant")
+    private Integer idEtudiant;
+    private String prenomE;
+    private String nomE;
+    @Enumerated(EnumType.STRING)
+    private Option option;
+
+}
